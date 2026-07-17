3c201a1d8f66e608c09694dc68a1f08f5811b905
Pixayo <kaio.rodrigo729@gmail.com>
2026-07-17 12:12:23 -0300

    update: draw empty maze to screen

---
+++ src/main.cpp
@@ -0,0 +1,98 @@
+#include "raylib.h"
+
+#define HEIGHT    30
+#define WIDTH     30
+#define CELL_SIZE 16
+#define MARGIN    CELL_SIZE * 2
+
+
+class Cell 
+{
+public:
+    int x;
+    int y;
+    bool isWall = false;
+    bool wasVisited = false; 
+
+    Cell() = default;
+    Cell(int x, int y) : x(x), y(y) {}
+};
+
+void initialize_grid(Cell grid[HEIGHT][WIDTH]);
+void draw_maze(Cell grid[HEIGHT][WIDTH]);
+bool isMazeBorder(Cell cell);
+
+int main()
+{
+    const int screenWidth = WIDTH * CELL_SIZE + (2 * MARGIN);
+    const int screenHeight = HEIGHT * CELL_SIZE + (2 * MARGIN);
+    
+    InitWindow(screenWidth, screenHeight, "maze-with-raylib");
+    SetTargetFPS(60);
+
+    Cell grid[HEIGHT][WIDTH];
+    initialize_grid(grid);
+
+    while (!WindowShouldClose()) 
+    {
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+        draw_maze(grid);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
+
+void initialize_grid(Cell grid[HEIGHT][WIDTH])
+{
+    for(int i = 0; i < HEIGHT; i++) 
+    {
+        for(int j = 0; j < WIDTH; j++)
+        {
+            Cell cell = Cell(i, j);
+
+            cell.isWall = isMazeBorder(cell);
+
+            grid[i][j] = cell;
+        }
+    }
+}
+
+void draw_maze(Cell grid[HEIGHT][WIDTH])
+{
+    for(int i = 0; i < HEIGHT; i++) 
+    {
+        for(int j = 0; j < WIDTH; j++)
+        {
+            Color color = grid[i][j].isWall ? GRAY : BLACK;
+
+            int posX = MARGIN + i * CELL_SIZE;
+            int posY = MARGIN + j * CELL_SIZE;
+
+            DrawRectangle(
+                posX, 
+                posY, 
+                CELL_SIZE, 
+                CELL_SIZE, 
+                color
+            );
+            DrawRectangleLines(
+                posX, 
+                posY, 
+                CELL_SIZE, 
+                CELL_SIZE, 
+                GRAY
+            );
+        }
+    }
+} 
+
+bool isMazeBorder(Cell cell)
+{
+    return ((cell.x == 0 || cell.x == HEIGHT - 1) || (cell.y == 0 || cell.y == WIDTH - 1));
+}
